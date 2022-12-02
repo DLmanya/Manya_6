4b905df45408ea918c21d68378cea77cfdc3ee1c
Manya <manjak29@yandex.ru>
2022-12-02 12:06:28 +0400

    задача 54

---
+++ Work_1/Program.cs
@@ -0,0 +1,52 @@
+﻿int[,] table = new int[3, 5];
+FillArrayRandom(table);
+PrintArray(table);
+SortToLower(table);
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Упорядоченный массив по убыванию в каждой строке: ");
+PrintArray(table);
+
+
+void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+        Console.ResetColor();
+    }
+}
